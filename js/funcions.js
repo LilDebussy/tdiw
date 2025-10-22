372601e4324732a161b6ff0f1009ea3e2c680fa1
Lluis05 <lssarries28@gmail.com>
2025-10-22 12:26:31 +0200

    js actualitzat

--- js/funcions.js
+++ js/funcions.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     $("#graus").change(function(){
         $("#mencions").load("./mencions.php?grau=" + $("#graus").val())
+
+        //opción 2
+        // $.ajax({url: "mencions.php?grau=" + $("#graus").val(), success:
+        // function(result){
+        //     $("#mencions").html(result);
+        // }});
     })
     
 });
