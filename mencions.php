813b0e8e8037d8a5f7df4906103a18e5e44fefba
Lluis05 <lssarries28@gmail.com>
2025-10-22 11:50:10 +0200

    canvis mencions.php

--- mencions.php
+++ mencions.php
@@ -1,3 +1,11 @@
 <?php
-//completa
+	include_once __DIR__ . "/connectaBD.php";
+	$con = connectaBD();
+	$query = "SELECT id,nom FROM mencions WHERE grau=" . $_REQUEST['grau'];
+	$resultSet = pg_query($con, $query) or die("Error sql");
+	$rows = pg_fetch_all($resultSet);
+	foreach($rows as $row){
+		echo "<option value='" . $row['id'] . "'>" . $row['nom'] . "</option>\n";
+	}
+	pg_close($con);
 ?>
