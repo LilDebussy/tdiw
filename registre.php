160d561399f01a2e1f2fa025c8ee34d480de14b2
Lluis05 <lssarries28@gmail.com>
2025-10-22 12:31:35 +0200

    finalitzat

--- registre.php
+++ registre.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
 <html>
-<!-- completa -->
+ <head>
+  <title> UAB/Enginyeria </title>
+  <meta charset="utf-8"/>
+ </head>
+ <body>
+  <h2>Resum de les teves dades:</h2>
+  <p>Nom: <?php echo $_REQUEST['nom']; ?></p>
+  <p>Grau: <?php echo $_REQUEST['grau']; ?></p>
+  <p>Menció: <?php echo $_REQUEST['mencio']; ?></p>
+ </body>
 </html>
